9694c116196755c717c6a2ec45e9f82d3cdc83bc
Guilherme Gonçalves Chagas <guilherme.chagas@programmers.com.br>
2022-05-17 10:52:03 -0300

    Inclusão da validação de notas de 1 a 10

--- Ex7/Ex7.cs
+++ Ex7/Ex7.cs
@@ -2,14 +2,29 @@
 
 double nota1, nota2, nota3, nota4, media;
 
-Console.WriteLine("Informe a 1ª nota: ");
-nota1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Informe a 2ª nota: ");
-nota2 = double.Parse(Console.ReadLine());
-Console.WriteLine("Informe a 3ª nota: ");
-nota3 = double.Parse(Console.ReadLine());
-Console.WriteLine("Informe a 4ª nota: ");
-nota4 = double.Parse(Console.ReadLine());
+do
+{
+    Console.WriteLine("Informe a 1ª nota: ");
+    nota1 = double.Parse(Console.ReadLine());
+} while (nota1 < 0 || nota1 > 10);
+
+do
+{
+    Console.WriteLine("Informe a 2ª nota: ");
+    nota2 = double.Parse(Console.ReadLine());
+} while (nota2 < 0 || nota2 > 10);
+
+do
+{
+    Console.WriteLine("Informe a 3ª nota: ");
+    nota3 = double.Parse(Console.ReadLine());
+} while (nota3 < 0 || nota3 > 10);
+
+do
+{
+    Console.WriteLine("Informe a 4ª nota: ");
+    nota4 = double.Parse(Console.ReadLine());
+} while (nota4 < 0 || nota4 > 10);
 
 media = (nota1 + nota2 + nota3 + nota4) / 4;
 
